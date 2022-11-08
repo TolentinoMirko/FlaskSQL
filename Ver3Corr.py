ea49bfa5aae4eb5a076851b41b70e95f1673b531
TolentinoMirko <tolentino.mirko@iisgalvanimi.edu.it>
2022-11-08 07:56:16 +0000

    3 da completare

--- Ver3Corr.py
+++ Ver3Corr.py
@@ -7,16 +7,20 @@ import pymssql
 
 @app.route('/', methods=['GET'])
 def home():
-   return redirect(url_for("InfoUser"))
+   return redirect(url_for("BestCustomers"))
 
 @app.route('/BestCustomers', methods=['GET'])
 def BestCustomers():
     conn = pymssql.connect(server = '213.140.22.237\SQLEXPRESS',user = 'tolentino.mirko',password = 'xxx123##',database = 'tolentino.mirko')
-    query = ""
-    return render_template("Ver3Corr/home.html")
+    query = "SELECT TOP 10 customers.customer_id, customers.first_name, customers.last_name ,SUM(list_price) as spesa_totale FROM sales.customers INNER JOIN sales.orders ON sales.customers.customer_id = sales.orders.customer_id INNER JOIN sales.order_items ON sales.order_items.order_id = sales.orders.order_id GROUP BY customers.customer_id, customers.first_name, customers.last_name ORDER BY SUM(list_price) DESC"
+    #nel group by aggiungo l'id(chiave),nome e cognome(così mostreremo anche loro) 
+    df = pd.read_sql(query,conn)
+    return render_template("Ver3Corr/home.html",nomiColonne = df.columns.values,dati = list(df.values.tolist()))
    
 
-
+@app.route('/TotaleOrdini', methods=['GET'])
+def TotaleOrdini(valore):
+   return
 
 
 
